42df06a4b3fcf71ed0f709884f63146765880db9
Bruce Hyslop <bruce.hyslop@csiro.au>
2023-11-29 14:12:56 +1100

    #180 use custom character data for symbols (without extra special characters)

--- grails-app/services/au/org/ala/userdetails/PasswordService.groovy
+++ grails-app/services/au/org/ala/userdetails/PasswordService.groovy
@@ -15,6 +15,7 @@
 
 package au.org.ala.userdetails
 
+import au.org.ala.auth.EnglishCustomCharacterData
 import au.org.ala.auth.PasswordPolicy
 import au.org.ala.cas.encoding.BcryptPasswordEncoder
 import au.org.ala.cas.encoding.LegacyPasswordEncoder
@@ -282,7 +283,7 @@ class PasswordService {
                     ruleGroup.rules.add(new CharacterRule(EnglishCharacterData.Digit, policy.charGroupMinDigit))
                 }
                 if (policy.charGroupMinSpecial > 0) {
-                    ruleGroup.rules.add(new CharacterRule(EnglishCharacterData.Special, policy.charGroupMinSpecial))
+                    ruleGroup.rules.add(new CharacterRule(EnglishCustomCharacterData.Special, policy.charGroupMinSpecial))
                 }
             }
 
