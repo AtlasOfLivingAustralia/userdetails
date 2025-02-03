e23ef9fc54908b738d74ebe2f0b7d37044b8d79c
yasima-csiro <yasima.kankanamge@csiro.au>
2025-02-04 09:42:20 +1100

    Update config

--- userdetails-plugin/grails-app/controllers/au/org/ala/userdetails/RegistrationController.groovy
+++ userdetails-plugin/grails-app/controllers/au/org/ala/userdetails/RegistrationController.groovy
@@ -508,7 +508,7 @@ class RegistrationController {
     private isMFAVisible(userInstance) {
         boolean isMFAEnabled = grailsApplication.config.getProperty('account.MFAenabled', Boolean, false)
         List MFAUnsupportedRoles = grailsApplication.config.getProperty('users.delegated-group-names', List, []).collect { jwtProperties.getRolePrefix() + it.toUpperCase()}
-        boolean hasMFAUnsupportedRoles = userInstance.roles.stream().every(userRoleRecord ->
+        boolean hasMFAUnsupportedRoles = userInstance.roles.stream().any(userRoleRecord ->
                 MFAUnsupportedRoles.contains(userRoleRecord.role.role))
 
         return isMFAEnabled && !hasMFAUnsupportedRoles
